59e39913f6f97cf97998c80012f0b2d318035f15
AidanKernahanUTS <aidan.kernahan@student.uts.edu.au>
2025-11-04 20:00:44 +1100

    Update todo-test.py

--- .github/scripts/todo-test.py
+++ .github/scripts/todo-test.py
@@ -1,1 +1,27 @@
+import unittest
+from todo import Task, TaskPool
+from io import StringIO
+import sys
 
+
+class TestTaskPool(unittest.TestCase):
+
+  def setup(self):
+    self.pool = TaskPool()
+
+  def tests_add_task(self):
+    task = Task("New Task")
+    self.pool.add_task(task)
+    self.assertEqual(len(self.pool.tasks), 1)
+
+  def test_get_open_tasks(self):
+    self.pool.populate()
+    open_tasks = self.pool.get_open_tasks()
+    self.assertEqual(len(open_tasks), 3)
+    self.assertTrue(all(task.status == "ToDo" for task in open_tasks))
+
+  def  test_get_done_tasks(self):
+    self.pool.populate()
+    done_tasks = self.pool.get_done_tasks()
+    self.assertEqual(len(done_tasks), 3)
+    self.assertTrue(all(task.status == "Done" for task in done_tasks))
